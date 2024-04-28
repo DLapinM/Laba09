1c79188e9c42d4cdc46dc479899ac22e72ec294f
DLapinM <autocad-profi@yandex.ru>
2024-04-28 15:14:58 +0300

    Laba 9 finish

--- src/itmo/Laba9/Laba9.java
+++ src/itmo/Laba9/Laba9.java
@@ -1,11 +1,67 @@
 package itmo.Laba9;
 
-import java.util.Calendar;
+import java.util.*;
 
 public class Laba9
 {
     public static void main(String[] args)
     {
+        /** Лаба 9. Упражнение 2. */
 
+        String[] names = {"Вася", "Петя", "Саша", "Маша", "Саша", "Ваня", "Петя"};
+        List<Object> myList = new ArrayList<Object>(Arrays.asList(names));
+
+        MyMethodsForCollections.printList(myList);
+
+        List<Object> myListUnique = MyMethodsForCollections.uniqueObjects(myList);
+
+        MyMethodsForCollections.printList(myListUnique);
+
+
+        /** Лаба 9. Упражнение 3. */
+
+        long start = System.currentTimeMillis();
+
+        List<Integer> listInteger1 = new ArrayList<Integer>();
+        MyMethodsForCollections.addManyIntegerElements(listInteger1);
+        List<Integer> listIntegerRezult1 = MyMethodsForCollections.getManySomeIntegerElementsFrom(listInteger1, 100000);
+
+        long finish = System.currentTimeMillis();
+        long elapsed = finish - start;
+        System.out.println("Затраченное время: " + elapsed + " милисекунд");
+        /** Затраченное время: 109 милисекунд */
+
+        start = System.currentTimeMillis();
+
+        LinkedList<Integer> listInteger2 = new LinkedList<Integer>();
+        MyMethodsForCollections.addManyIntegerElements(listInteger2);
+        List<Integer> listIntegerRezult2 = MyMethodsForCollections.getManySomeIntegerElementsFrom(listInteger2, 100000);
+
+        finish = System.currentTimeMillis();
+        elapsed = finish - start;
+        System.out.println("Затраченное время: " + elapsed + " милисекунд");
+        /** Затраченное время: 197978 милисекунд */
+
+        /** Второй вариант дольше, поскольку используется связанный список, а в связанном списке для произвольного
+        доступа к элементу придется перебирать элементы по одному до тех пор, пока мы не придем к нужному. */
+
+
+        /** Лаба 9. Упражнение 4. */
+
+        User user1 = new User("Вася");
+        User user2 = new User("Петя");
+        User user3 = new User("Саша");
+        User user4 = new User("Маша");
+        User user5 = new User("Ваня");
+
+        Map<User, Integer> map1 = new HashMap<User, Integer>();
+
+        map1.put(user1, 445);
+        map1.put(user2, 554);
+        map1.put(user3, 334);
+        map1.put(user4, 335);
+        map1.put(user5, 554);
+
+        MyMethodsForCollections.inputUsernameAndPrintPoints(map1);
     }
 }
--- src/itmo/Laba9/MyMethodsForCollections.java
+++ src/itmo/Laba9/MyMethodsForCollections.java
@@ -1,6 +1,130 @@
 package itmo.Laba9;
 
+import java.util.*;
+
 public class MyMethodsForCollections
 {
+    public static List<Object> uniqueObjects (List<Object> list1)
+    {
+        HashSet<Object> set1 = new HashSet<>();
 
+        for (Object el : list1)
+        {
+            set1.add(el);
+        }
+
+        List<Object> list2 = new ArrayList<Object>();
+
+        for (Object el : set1)
+        {
+            list2.add(el);
+        }
+
+        Collections.sort(list2, Collections.reverseOrder());
+        Collections.reverse(list2);
+
+        return list2;
+    }
+
+    public static void printList(List<Object> list1)
+    {
+        for (Object el : list1)
+        {
+            System.out.println(el);
+        }
+
+        System.out.println("");
+    }
+
+    public static void addManyIntegerElements (List<Integer> list1)
+    {
+        for (Integer i = 1; i <= 1000000; i++)
+        {
+            list1.add(i);
+        }
+    }
+
+    public static void addManyIntegerElements (LinkedList<Integer> list1)
+    {
+        for (Integer i = 1; i <= 1000000; i++)
+        {
+            list1.add(i);
+        }
+    }
+
+    public static Integer getSomeIntegerElementFrom (List<Integer> list1)
+    {
+        Random rand = new Random();
+
+        int n = rand.nextInt(list1.size()-1);
+
+        if (n < 0) return null;
+        else return list1.get(n);
+    }
+
+    public static Integer getSomeIntegerElementFrom (LinkedList<Integer> list1)
+    {
+        Random rand = new Random();
+
+        int n = rand.nextInt(list1.size()-1);
+
+        if (n < 0) return null;
+        else return list1.get(n);
+    }
+
+    public static List<Integer> getManySomeIntegerElementsFrom (List<Integer> list1, int n)
+    {
+        List<Integer> list2 = new ArrayList<>();
+
+        for (int i = 0; i < n; i++)
+        {
+            Integer j = getSomeIntegerElementFrom(list1);
+            list2.add(j);
+        }
+
+        return list2;
+    }
+
+    public static List<Integer> getManySomeIntegerElementsFrom (LinkedList<Integer> list1, int n)
+    {
+        List<Integer> list2 = new ArrayList<>();
+
+        for (int i = 0; i < n; i++)
+        {
+            Integer j = getSomeIntegerElementFrom(list1);
+            list2.add(j);
+        }
+
+        return list2;
+    }
+
+    public static Integer getPointsFromMapByUsername (Map<User, Integer> map1, String username)
+    {
+        for (User el : map1.keySet())
+        {
+            if (el.getName().equals(username)) return map1.get(el);
+        }
+
+        return null;
+    }
+
+    public static void inputUsernameAndPrintPoints(Map<User, Integer> map1)
+    {
+        System.out.println("Введите имя пользователя:");
+
+        Scanner scaner = new Scanner(System.in);
+
+        String username = scaner.nextLine();
+
+        Integer rezInt = getPointsFromMapByUsername(map1, username);
+
+        if (null != rezInt)
+        {
+            System.out.println("Количество очков пользователя " + username + ": " + rezInt);
+        }
+        else
+        {
+            System.out.println("Пользователя с именем " + username + " не существует");
+        }
+    }
 }
